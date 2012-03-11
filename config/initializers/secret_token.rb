9c7446d958d61e021d69fb79d3f35585f86e29e4
Thomas Borg Salling <tbsalling@tbsalling.dk>
2012-03-11 20:59:47 +0100

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Coolcase::Application.config.secret_token = '47e70dbff968f2881cc0220d0d84b7b148d316f556e26f477356a0d6d2d03b2cd5ed051b3bddc8eae4c8c26bbb722c44e90febe27d90a88172375c8911b100ba'
